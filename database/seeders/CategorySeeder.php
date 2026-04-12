949c436503374546f349738a031a89e34a7fdfcf
Momar TALL <tonemail@gmail.com>
2026-04-12 00:18:20 +0000

    fix: restore syntax in CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,21 +14,25 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
+            [
                 'name' => 'Vaches', 
                 'icon' => 'fas fa-cow', 
                 'slug' => 'vaches',
                 'image' => 'https://images.unsplash.com/photo-1546445317-29f4545e9d53?q=80&w=1200'
             ],
+            [
                 'name' => 'Chevaux', 
                 'icon' => 'fas fa-horse', 
                 'slug' => 'chevaux',
                 'image' => 'https://images.unsplash.com/photo-1553284965-83fd3e82fa5a?q=80&w=1200'
             ],
+            [
                 'name' => 'Poulets', 
                 'icon' => 'fas fa-egg', 
                 'slug' => 'poulets',
                 'image' => 'https://images.unsplash.com/photo-1569337776101-921350a4d53c?q=80&w=1200'
             ],
+            [
                 'name' => 'Aliments de bétail', 
                 'icon' => 'fas fa-seedling', 
                 'slug' => 'aliments',
